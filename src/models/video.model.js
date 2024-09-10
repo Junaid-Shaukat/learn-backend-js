850bd06865e2f8f1445c514cd21e48d2f241e706
Junaid <junaiddshaukat@gmail.com>
2024-09-10 14:26:56 +0500

    video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -2,8 +2,24 @@ import mongoose, {Schema} from "mongoose";
 
 const videoSchema = new Schema({
     videoFile:{
+        type:String, //cloudniary Url
+        required:true,
+    },
+    thumbnail:{
+        type:String, //cloudniary Url
+        required:true,
+    },
+    title:{
         type:String,
         required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    duration:{
+        type:Number, //cloudniary Url
+        required:true
     }
 },{timestamps:true})
 
